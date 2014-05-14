e422bed6099594bdc9f867d5304fd55a056619a0
zanema <amenaz1@gmail.com>
2014-05-14 19:25:07 -0400

    SIG-8: Upgrade version and get rid of wildcard styling and change image storage path

--- app/models/signature/signature_doc_image.rb
+++ app/models/signature/signature_doc_image.rb
@@ -7,7 +7,9 @@ module Signature
     included do
       attr_accessible :page, :signature_document, :image
       # TODO: don't store in public folder - create an authenticated route for downloading files
-      has_attached_file :image
+      has_attached_file :image,
+                        :url => "/system/:rails_env/:class/:attachment/:id/:filename"
+
       validates_attachment_content_type :image, :content_type => /^image\/(png|gif|jpeg)/
 
       RES_SCALE = 3
